4bd480540316585fedce5108373c4492662cd114
Micro-Master7 <107751250+Micro-Master7@users.noreply.github.com>
2022-06-18 21:38:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(min)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(max)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showNumber(currenttemperature)
+})
+let min = 0
+let max = 0
+let currenttemperature = 0
+currenttemperature = input.temperature()
+max = currenttemperature
+min = currenttemperature
+basic.forever(function () {
+    basic.showString(".")
+    currenttemperature = input.temperature()
+    if (currenttemperature < min) {
+        min = currenttemperature
+    }
+    if (currenttemperature > max) {
+        max = currenttemperature
+    }
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+})
